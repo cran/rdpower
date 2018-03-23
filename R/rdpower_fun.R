05f95593f1947eb00f242ee778324d38d450949a
Gonzalo Vazquez-Bare <gvazquez@umich.edu>
2018-03-23 12:25:38 +0000

    version 0.1

---
+++ R/rdpower_fun.R
@@ -0,0 +1,73 @@
+###################################################################
+# Auxiliary functions for rdpower
+# !version 0.1 05-Mar-2018
+# Authors: Matias Cattaneo, Rocio Titiunik, Gonzalo Vazquez-Bare
+###################################################################
+
+
+#################################################################
+# Power function
+#################################################################
+
+rdpower.powerfun = function(n,tau,stilde,z){
+  
+  x = 1 - pnorm(sqrt(n)*tau/stilde+z) + pnorm(sqrt(n)*tau/stilde-z)
+  
+  return(x)
+  
+}
+
+#################################################################
+# Power function derivative
+#################################################################
+
+rdpower.powerfun.dot = function(n,tau,stilde,z){
+  
+  x = (dnorm(sqrt(n)*tau/stilde-z)-dnorm(sqrt(n)*tau/stilde+z))*tau/(2*stilde*sqrt(n))
+  
+  return(x)
+  
+}
+
+#################################################################
+# Newton-Raphson
+#################################################################
+
+rdpower.powerNR = function(x0,tau,stilde,z,beta){
+  
+  tol = 1
+  iter = 0
+  
+  while (tol>.Machine$double.eps){
+    iter = iter + 1
+    k = 1
+    # Check if derivative at x0 is too small
+    while (rdpower.powerfun.dot(x0,tau,stilde,z)<.00001){
+      x0 = 1.2*x0*(rdpower.powerfun(x0,tau,stilde,z)<=beta) + .8*x0*(rdpower.powerfun(x0,tau,stilde,z)>beta)
+      iter = iter + 1
+    }
+    
+    x1 = x0 - (rdpower.powerfun(x0,tau,stilde,z)-beta)/rdpower.powerfun.dot(x0,tau,stilde,z)
+    
+    # Check if x1 is negative or too small
+    while (x1<2){
+      x1 = x0 - k*(rdpower.powerfun(x0,tau,stilde,z)-beta)/rdpower.powerfun.dot(x0,tau,stilde,z)
+      k = k/2
+      iter = iter + 1
+    }
+    
+    tol = abs(rdpower.powerfun(x1,tau,stilde,z)-beta)
+    x0 = x1
+    
+  }
+  
+  b = rdpower.powerfun(x1,tau,stilde,z)
+  m = ceiling(x1)
+  
+  output = list(m = m,
+                iter = iter,
+                powercheck = b)
+  
+  return(output)
+}
+
---
+++ R/rdpower_package.R
@@ -0,0 +1,39 @@
+###################################################################
+# rdpower: Power calculations for RD designs
+# !version 0.3 13-Mar-2018
+# Authors: Matias Cattaneo, Rocio Titiunik, Gonzalo Vazquez-Bare
+###################################################################
+
+
+#' rdpower: Power and Sample Size Calculations for RD Designs
+#'
+#' The regression discontinuity (RD) design is a popular quasi-experimental design
+#' for causal inference and policy evaluation. The \code{'rdpower'} package provides tools
+#' to perform power and sample size calculations in RD designs: \code{\link{rdpower}()} calculates
+#' the power of an RD design and \code{\link{rdsampsi}()} calculates the required sample size to achieve
+#' a desired power. For more details, and related \code{Stata} and \code{R} packages
+#' useful for analysis of RD designs, visit \url{https://sites.google.com/site/rdpackages}.
+#'
+#' @author
+#' Matias Cattaneo, University of Michigan. \email{cattaneo@umich.edu}
+#'
+#' Rocio Titiunik, University of Michigan. \email{titiunik@umich.edu}
+#'
+#' Gonzalo Vazquez-Bare, University of Michigan. \email{gvazquez@umich.edu}
+#'
+#' @references
+#'
+#' M.D. Cattaneo, R. Titiunik and G. Vazquez-Bare. (2018). \href{https://sites.google.com/site/rdpackages/rdpower/Cattaneo-Titiunik-VazquezBare_2018_Stata.pdf}{Power Calculations for Regression Discontinuity Designs}. \emph{Working paper, University of Michigan}.
+#'
+#' @importFrom graphics abline
+#' @importFrom graphics curve
+#' @importFrom graphics legend
+#' @importFrom graphics title
+#' @importFrom stats dnorm
+#' @importFrom stats pnorm
+#' @importFrom stats qnorm
+#' @importFrom stats sd
+#'
+#'
+#' @aliases rdpower_package
+"_PACKAGE"
